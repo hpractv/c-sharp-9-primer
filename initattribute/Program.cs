0685743f7f837632745674f5f694b8cbf70e893f
presza <preston.zaugg@extendhealth.com>
2020-11-13 10:36:44 -0700

    new files

---
+++ initattribute/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace c_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("No Skippy");
+            NoSkippy();
+            for (int x = 0; x < 2; x++)
+            {
+                Console.WriteLine("\nSkippy");
+                Skippy();
+            }
+        }
+
+
+        [System.Runtime.CompilerServices.SkipLocalsInitAttribute]
+        static void Skippy()
+        {
+            Span<int> nums = stackalloc int[10];
+            foreach (var x in nums) Console.WriteLine(x);
+        }
+
+        static void NoSkippy()
+        {
+            Span<int> nums = stackalloc int[10];
+            foreach (var x in nums) Console.WriteLine(x);
+        }
+    }
+}
